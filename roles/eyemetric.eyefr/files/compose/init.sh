fec4e6cbf34e66147682b694835b81b058847f04
ryan lee martin <brightblade42@protonmail.com>
2024-08-21 20:32:00 -0600

    removed old files. fixed typos

--- roles/eyemetric.eyefr/files/compose/init.sh
+++ roles/eyemetric.eyefr/files/compose/init.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
+VERSION="1.6.2"
 
 start_docker() {
-    ansible-playbook /opt/eyemetric/fr/compose/paravision-dkup.yml
-    ansible-playbook /opt/eyemetric/fr/compose/safr-dkup.yml
+    ansible-playbook /opt/eyemetric_${VERSION}/fr/compose/paravision-dkup.yml
+    ansible-playbook /opt/eyemetric_${VERSION}/fr/compose/safr-dkup.yml
 }
 
 start_podman() {
-    ansible-playbook /opt/eyemetric/fr/compose/paravision-pdup.yml
-    ansible-playbook /opt/eyemetric/fr/compose/safr-pdup.yml
+    ansible-playbook /opt/eyemetric_${VERSION}/fr/compose/paravision-pdup.yml
+    ansible-playbook /opt/eyemetric_${VERSION}/fr/compose/safr-pdup.yml
 }
 
 start_docker_cpu() {
-    ansible-playbook /opt/eyemetric/fr/compose/paravision-dkup-cpu.yml
-    ansible-playbook /opt/eyemetric/fr/compose/safr-dkup-cpu.yml
+    ansible-playbook /opt/eyemetric_${VERSION}/fr/compose/paravision-dkup-cpu.yml
+    ansible-playbook /opt/eyemetric_${VERSION}/fr/compose/safr-dkup-cpu.yml
 }
 
 start_podman_cpu() {
-    ansible-playbook /opt/eyemetric/fr/compose/paravision-pdup-cpu.yml
-    ansible-playbook /opt/eyemetric/fr/compose/safr-pdup-cpu.yml
+    ansible-playbook /opt/eyemetric_${VERSION}/fr/compose/paravision-pdup-cpu.yml
+    ansible-playbook /opt/eyemetric_${VERSION}/fr/compose/safr-pdup-cpu.yml
 }
 
 # If CONTAINER_ENGINE is set, check its value
@@ -28,3 +29,6 @@ elif [ "$CONTAINER_ENGINE" == "podman" ]; then
 else
         start_podman
 fi
+~
+~
+~
